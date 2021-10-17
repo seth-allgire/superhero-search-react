7b52b02bad896ec767918840e19aa0ef6244481d
Seth Allgire <seth.allgire@gmail.com>
2021-10-17 17:23:12 -0500

    adds account creation functionality to back end

--- server/models/users.models.js
+++ server/models/users.models.js
@@ -1,3 +1,37 @@
-function signup() {}
+const query = require("../config/mysql.conf");
+const bcrypt = require("bcrypt");
 
-function login() {}
+async function signup(res, username, password) {
+  try {
+    const [user] = await query("SELECT * FROM users WHERE users.username = ?", [
+      username,
+    ]);
+    if (user) {
+      return res.send({
+        success: false,
+        error: "Username already taken",
+        data: null,
+      });
+    }
+    const hash = await bcrypt.hash(password, 10);
+    await query("INSERT INTO users (username, password) VALUES (?,?)", [
+      username,
+      hash,
+    ]);
+    return res.send({
+      success: true,
+      error: null,
+      data: "Successfully signed up?",
+    });
+  } catch (e) {
+    return res.send({
+      success: false,
+      error: "Something went wrong. Please try again.",
+      data: null,
+    });
+  }
+}
+
+async function login(res, username, password) {}
+
+module.exports = { signup, login };
--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const router = express.Router();
+const { signup } = require("../models/users.models");
 
 router.post("/signup", (req, res) => {
-  return res.send("SIGNUP....");
+  const { username, password } = req.body;
+  if (
+    !username ||
+    !password ||
+    username.length < 4 ||
+    username.length > 20 ||
+    password.length < 8 ||
+    password.length > 20
+  ) {
+    return res.send({
+      success: false,
+      error: "Invalid data provided",
+      data: null,
+    });
+  }
+  signup(res, username, password);
 });
 
 router.post("/login", (req, res) => {
